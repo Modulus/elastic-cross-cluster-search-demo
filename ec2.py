03b387617166f7bc54c75a954dfce1df05769114
user <user@bgomac-user23.local>
2018-04-26 15:07:21 +0200

    Added fabric and boto libs, working. Kibana complains about x-pack, even if it is deactivated

---
+++ ec2.py
@@ -0,0 +1,64 @@
+import boto3
+from fabric.decorators import task
+
+
+@task
+def create():
+    # ubuntu 16.04 image
+    image_id = "ami-f90a4880"
+
+    ec2 = boto3.resource("ec2")
+    print("Checking for existing instances firsts")
+
+    instances = ec2.instances.filter(Filters=
+                                     [
+                                         {"Name": "instance-state-name", "Values": ["running"]}
+                                     ])
+
+    instance = [instance for instance in instances]
+    if len(instance) > 1:
+        print("Instances already created, skipping")
+    else:
+        print("No instances found")
+        print("Creating instances")
+
+        for i in range(1,2):
+            instance_name = "node-{}".format(i)
+            print("Creating instance {}".format(instance_name))
+            ec2.create_instances(
+                ImageId=image_id,
+                MinCount=1, MaxCount=1,
+                InstanceType="t2.large",
+                KeyName="id_rsa_tv2_aws",
+                TagSpecifications=[
+                    {
+                        'ResourceType': 'instance',
+                        'Tags': [{
+                            'Key': 'Name',
+                            'Value': '{}'.format(instance_name)
+                        },
+                            {
+                                "Key": "Type",
+                                "Value": "elastic"
+                            }
+                        ]
+                    }
+                ])
+
+@task
+def delete():
+    ec2 = boto3.resource("ec2")
+    instances = ec2.instances.filter(Filters=[{"Name": "instance-state-name", "Values": ["running"]}])
+
+    instance_ids = [instance.id for instance in instances]
+    print("Preparing to terminate")
+
+    for instance_id in instance_ids:
+        print(instance_id)
+
+    print("Stopping insances and terminating")
+
+    ec2.instances.filter(InstanceIds=instance_ids).stop()
+    ec2.instances.filter(InstanceIds=instance_ids).terminate()
+
+
---
+++ fabfile.py
@@ -0,0 +1,62 @@
+import boto3
+from fabric.api import env, roles, task, run
+from fabric.operations import put
+
+ec2 = boto3.resource("ec2")
+instances = ec2.instances.filter(Filters=[{"Name": "instance-state-name", "Values": ["running"]}])
+
+public_ips = [instance.public_ip_address for instance in instances]
+
+
+env.roledefs["nodes"] = public_ips
+#env.roledefs["search"] = public_ips[3:4]
+#env.roledefs["elastic"] = public_ips[0:3]
+
+env.user = "ubuntu"
+env.key_filename = "/Users/user/.ssh/id_rsa_tv2"
+
+@task
+def nodes():
+    print("nodes available")
+    for instance in [instance for instance in instances]:
+        name = None
+        for tag in instance.tags:
+            if tag["Key"] == "Name":
+                name = tag["Value"]
+        print("id: {}, Name: {}, public_ip: {}, private_ip: {}".format(instance.id, name, instance.public_ip_address, instance.private_ip_address))
+@task
+def check():
+    run("echo 'pong'")
+
+@roles("nodes")
+@task
+def docker():
+    run("sudo apt-get update")
+    run("sudo apt-get remove docker docker-engine docker.io")
+    run("sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common")
+    run("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+    run("sudo apt-key fingerprint 0EBFCD88")
+    run("sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable'")
+    run("sudo apt-get update")
+    run("sudo apt-get -y install docker-ce")
+    run("sudo docker pull docker.elastic.co/elasticsearch/elasticsearch-oss:6.2.4")
+
+    put("./docker-compose.yml", "/home/ubuntu/docker-compose.yml")
+    put("./config/elastic.yml", "/home/ubuntu/config/elastic.yml")
+    put("./config/kibana.yml", "/home/ubuntu/config/kibana.yml")
+    put("./config/search.yml", "/home/ubuntu/config/search.yml")
+
+    # Install docker compose
+    run("sudo apt-get install python-pip")
+    run("pip install docker-compose")
+
+
+@roles("nodes")
+@task
+def config():
+    put("./docker-compose.yml", "/home/ubuntu/docker-compose.yml")
+    put("./config/elastic.yml", "/home/ubuntu/config/elastic.yml")
+    put("./config/kibana.yml", "/home/ubuntu/config/kibana.yml")
+    put("./config/search.yml", "/home/ubuntu/config/search.yml")
+
+import ec2
